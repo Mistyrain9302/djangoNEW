5404ddd24cfa87f85d14bfe86583d8dcf1127aba
Mistyrain <Mistyrain@naver.com>
2023-02-17 12:57:41 +0900

    230217_필요없는것 삭제

--- mysite/simu/urls.py
+++ mysite/simu/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from simu.dash_apps.finished_apps import simple_example, dist, cluster, regressor
+from simu.dash_apps.finished_apps import dist, cluster, regressor
 from . import views
 
 app_name = 'simu'
@@ -9,6 +9,4 @@ urlpatterns = [
     path('cluster',views.cluster, name='cluster'),
     path('regressor',views.regressor, name='regressor'),
     path('scores/',views.scores, name='scores'),
-    path('classify/',views.classify,name='classify'),
-    path('regress/',views.regress,name='regress'),
 ]
