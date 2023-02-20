d982712ebec314472df90cd30ba2a5d6c37d4156
Mistyrain <Mistyrain@naver.com>
2023-02-20 18:30:44 +0900

    230220_prod.py 변경

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -2,6 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS = ['15.164.66.24']
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = "/home/ubuntu/projects/djangoNEW/mysite/static/"
 STATICFILES_DIRS = []
 DEBUG = False
