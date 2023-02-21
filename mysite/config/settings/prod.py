1bc4aa7c99a7854e0bc070590269daa65439c217
Mistyrain <Mistyrain@naver.com>
2023-02-21 11:50:01 +0900

    Revert "prod_py의 static root 삭제"
    This reverts commit 3cd16f6795eddb10e35d9a302f14f73c70fc7dad.

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -2,10 +2,10 @@ from .base import *
 
 ALLOWED_HOSTS = ['15.164.66.24']
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-#     '/home/ubuntu/projects/djangoNEW/mysite/static/',
-# ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    '/home/ubuntu/projects/djangoNEW/mysite/static/',
+]
 
 DEBUG = False
